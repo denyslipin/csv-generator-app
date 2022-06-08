1824fa07fab43b916246b5c2809bde8a3b9596df
Denys Lipin <lipin.transstream@gmail.com>
2022-06-09 00:59:31 +0300

    Added static_root to settings

--- app/settings.py
+++ app/settings.py
@@ -147,6 +147,8 @@ STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
 
+STATIC_ROOT = "staticfiles/"
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
